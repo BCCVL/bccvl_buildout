99ca92267ed6fc5f18afd7101e5fc43548547715
Gerhard Weis <g.weis@griffith.edu.au>
2014-04-16 08:48:34 +1000

    Revert "Move buildout configs to configs/" because it breaks CI environment
    This reverts commit 50b47794a93cfbce56479130b5a364c7294661be.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -24,7 +24,7 @@ echo "Python version:"
 "$PYTHON" --version
 
 echo "Configuring buildout"
-cp configs/jenkins.cfg buildout.cfg
+cp buildout.cfg.jenkins buildout.cfg
 
 echo "Run bootstrap and then buildout"
 "$PIP" install distribute --upgrade
