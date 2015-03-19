3acda4043241fe431fb7304cf4d815d595019724
Gerhard Weis <g.weis@griffith.edu.au>
2015-03-19 12:44:43 +1000

    pin setuptools and buildout versions for jenkins build

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -17,8 +17,9 @@ cd $WORKSPACE
 echo "Setting up virtualenv in $WORKSPACE"
 curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-12.0.7.tar.gz
 tar -xvzf virtualenv-12.0.7.tar.gz
-python virtualenv-12.0.7/virtualenv.py --no-pip -p /usr/bin/python2.7 .
+python virtualenv-12.0.7/virtualenv.py -p /usr/bin/python2.7 .
 source bin/activate
+easy_install setuptools==0.9.8
 
 echo "Python version:"
 "$PYTHON" --version
@@ -27,7 +28,7 @@ echo "Configuring buildout"
 cp buildout.cfg.jenkins buildout.cfg
 
 echo "Run bootstrap and then buildout"
-"$PYTHON" bootstrap.py
+"$PYTHON" bootstrap.py -v 2.2.1
 $BUILDOUT
 
 Xvfb :10 -screen 0 1366x768x24 -ac &
