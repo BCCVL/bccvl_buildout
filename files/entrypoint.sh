6fb1a8d35d991f3741ecc356cab31fbef9e25995
Gerhard Weis <g.weis@griffith.edu.au>
2016-08-16 20:35:21 +1000

    fix entrypoint

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,19 +2,19 @@
 
 # make sure config files are in place and readable
 if [ -e "${BCCVL_ETC}/zope/zope.conf" ] ; then
-    ln -sf "${BCCVL_ETC}/zope/zope.conf" "${BCCVL_HOME}/parts/instance/etc/zope.conf"
-    chmod 600 "${BCCVL_ETC}/zope/zope.conf"
-    chown ${BCCVL_USER}:${BCCVL_USER} "${BCCVL_ETC}/zope/zope.conf"
+    cp  "${BCCVL_ETC}/zope/zope.conf" "${BCCVL_HOME}/parts/instance/etc/zope.conf"
+    chmod 600 "${BCCVL_HOME}/parts/instance/etc/zope.conf"
+    chown ${BCCVL_USER}:${BCCVL_USER} "${BCCVL_HOME}/parts/instance/etc/zope.conf"
 fi
 if [ -e "${BCCVL_ETC}/wsgi/zope.wsgi" ] ; then
-    ln -sf "${BCCVL_ETC}/wsgi/zope.wsgi" "${BCCVL_HOME}/zope.wsgi"
-    chmod 600 "${BCCVL_ETC}/wsgi/zope.wsgi"
-    chown ${BCCVL_USER}:${BCCVL_USER} "${BCCVL_ETC}/wsgi/zope.wsgi"
+    cp  "${BCCVL_ETC}/wsgi/zope.wsgi" "${BCCVL_HOME}/zope.wsgi"
+    chmod 600 "${BCCVL_HOME}/zope.wsgi"
+    chown ${BCCVL_USER}:${BCCVL_USER} "${BCCVL_HOME}/zope.wsgi"
 fi
 if [ -e "${BCCVL_ETC}/bccvl/bccvl.ini" ] ; then
-    ln -sf "${BCCVL_ETC}/bccvl/bccvl.ini" "${BCCVL_HOME}/bccvl.ini"
-    chmod 600 "${BCCVL_ETC}/wsgi/bccvl.ini"
-    chown ${BCCVL_USER}:${BCCVL_USER} "${BCCVL_ETC}/wsgi/bccvl.ini"
+    cp "${BCCVL_ETC}/bccvl/bccvl.ini" "${BCCVL_HOME}/bccvl.ini"
+    chmod 600 "${BCCVL_HOME}/bccvl.ini"
+    chown ${BCCVL_USER}:${BCCVL_USER} "${BCCVL_HOME}/bccvl.ini"
 fi
 # change permission of storage location
 if [ -e "${BCCVL_VAR}" ] ; then
